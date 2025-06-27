d015bb32ff4f1fb687258af4e3cec5833af7ae1d
Arnav <120742713+arnav-k6@users.noreply.github.com>
2025-06-27 15:18:25 -0300

    Update array_sorting.py

--- array_sorting.py
+++ array_sorting.py
@@ -4,3 +4,13 @@ def bubble_sort(array):
             if array[j] > array[j+1]:
                 array[j],array[j+1] = array[j+1],array[j]
 
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        value = arr[i]
+        j = i-1
+
+        while j>=0 and arr[j] > value:
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = value
+
